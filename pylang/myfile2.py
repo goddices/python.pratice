9abc0ded7208d52a0155528c8470d1c446fc502c
goddices <qq470857673@gmail.com>
2018-06-20 18:09:47 +0800

    1

--- pylang/myfile2.py
+++ pylang/myfile2.py
@@ -4,10 +4,19 @@ import sys
 output_file  = sys.argv[1]
 shortvin = sys.argv[2]
 longvin = sys.argv[3]
+start = sys.argv[4]
+end = sys.argv[5]
  
 
-template = "insert into tab1 values('%s','%s','%s')"%("aab","aaaa","cccc")
+template = '''
+insert into  CDContract  (ShortVIN,LongVIN,OfferId,OfferName,StartDate,EndDate)
+values('{0}','{1}','CN0007',        'NBT 6AK Retail - Premium Package CN',  '2018-05-10','{2}') ,
+	  ('{0}','{1}','CNN008',		'E.Nav Retail - Premium Package',		'2018-05-10','{3}') , 
+	  ('{0}','{1}','BMPHEV_BASE_CN','BMW Phev Entry Package',				'2018-05-10','{4}') , 
+	  ('{0}','{1}','BMW_PREMIUM_CN','1Y Premium Package Upselling',			'2018-05-10','{5}');
+'''
+
 f1 = open(output_file,"w+")
 f1.write(template)
 f1.close()
-print("done")
+ 
