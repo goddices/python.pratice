a62ba94a44bab2f1f3b4520473e1cc11c3741e9a
goddices <qq470857673@gmail.com>
2018-08-23 18:02:39 +0800

    data....

--- myscrapy/tutorial/tutorial/spiders/loldy.py
+++ myscrapy/tutorial/tutorial/spiders/loldy.py
@@ -3,27 +3,27 @@ from tutorial.items import  LoLdyItem
 
 class LoLdySpider(scrapy.Spider):
     name = "loldy"
-    # start_urls = [
-    #     'file://c/users/qxu8502/desktop/loldownload.html',
-    # ]
+    start_urls = [
+        'https://www.loldytt.com/Dongzuodianying/MTYJ/',
+    ]
 
 
-    def start_requests(self): 
-        source_table = [ 
-                        # ('Dongzuodianying',99), 
-                        # ('Kehuandianying',31), 
-                        # ('Kongbudianying',83),
-                        # ('Xijudianying',121),
-                        # ('Aiqingdianying',54),
-                        # ('Juqingdianying',276),
-                        # ('Zhanzhengdianying',13),
-                        # ('Anime',118),
-                        ('Zuixinmeiju',102) 
-                        ]
-        for row in source_table:
-            for i in range(row[1]): 
-                yield scrapy.Request('https://www.loldytt.com/%s/chart/%d.html'%(row[0],i+1), self.parse_list) 
-               
+    # def start_requests(self): 
+        # source_table = [ 
+        #                 # ('Dongzuodianying',99), 
+        #                 # ('Kehuandianying',31), 
+        #                 # ('Kongbudianying',83),
+        #                 # ('Xijudianying',121),
+        #                 # ('Aiqingdianying',54),
+        #                 # ('Juqingdianying',276),
+        #                 # ('Zhanzhengdianying',13),
+        #                 # ('Anime',118),
+        #                 ('Zuixinmeiju',102) 
+        #                 ]
+        # for row in source_table:
+        #     for i in range(row[1]): 
+        #         yield scrapy.Request('https://www.loldytt.com/%s/chart/%d.html'%(row[0],i+1), self.parse_list) 
+         
 
     def parse_list(self, response):
         links = response.xpath('//div[@class="box3_mid"]/ul/li/div/a/@href').extract()
@@ -37,14 +37,14 @@ class LoLdySpider(scrapy.Spider):
             f3 = response.xpath('//div[@class="haibao"]/a/img/@src').extract()
             f4 = response.xpath('//div[@class="zhuyan"]/ul/li/text()').extract()
             f5 = response.xpath('//div[@class="neirong"]/p/text()').extract()
-            f6 = response.xpath('//ul[@class="downurl"]/li/a/@href').extract() 
+            f6 = response.xpath('//ul[@class="downurl"]/li/a/@href | //ul[@class="downurl"]/li/a/@title').extract() 
             f7 = response.xpath('//ul[@class="lm"]/h1/p/text()').extract() 
             yield LoLdyItem( 
                 title = f1[0] if len(f1)>0 else 'null',
                 tags = str(f2[0]).strip('\r\n') if len(f2)>1 else 'null'  ,
                 image =f3[0] if len(f3)>0 else 'null' ,
                 actors =f4[0] if len(f4)>0 else 'null' ,
-                content =f5[0] if len(f5)>0 else 'null',
+                content = str(f5),
                 download = str(f6) ,
                 datetime  = f7[0] if len(f7)>0 else 'null'
             )
